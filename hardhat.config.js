512c5701e601256754bd47690e4b61d9c285c348
Joel Gustafson <joelg@mit.edu>
2021-11-18 19:29:11 -0500

    rename hash to sign

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
-require('hardhat-circom');
+require("hardhat-circom")
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.7",
-  circom: {
-    inputBasePath: "./circuits",
-    ptau: "pot15_final.ptau",
-    circuits: [{ name: "hash" }],
-  },
-};
+	solidity: "0.6.7",
+	circom: {
+		inputBasePath: "./circuits",
+		ptau: "pot15_final.ptau",
+		circuits: [{ name: "sign" }],
+	},
+}
